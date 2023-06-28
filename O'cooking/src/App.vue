5e194c7d2f906a7076b1965ce0c5f7b442fd670e
GaelleBriet <gaelle.briet@protonmail.com>
2023-06-28 15:04:08 +0200

    👌 IMPROVE: login value

--- O'cooking/src/App.vue
+++ O'cooking/src/App.vue
@@ -2,12 +2,21 @@
 import HeaderComponent from './components/layout/HeaderComponent.vue'
 import FooterComponent from './components/layout/FooterComponent.vue'
 import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
 
+const isLoggedIn = ref(false)
+const login = () => {
+  isLoggedIn.value = true
+}
+const logout = () => {
+  isLoggedIn.value = false
+}
+console.log('est connecté : ', isLoggedIn.value)
 </script>
 
 <template>
   <HeaderComponent />
-  <RouterView />
+  <RouterView :isLoggedIn="isLoggedIn" @login="login" @logout="logout" />
   <FooterComponent />
 </template>
 
