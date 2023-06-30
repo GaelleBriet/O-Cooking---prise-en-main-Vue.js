530f1e40afb9a37032cc665c39c5243e6505486d
GaelleBriet <gaelle.briet@protonmail.com>
2023-06-30 13:49:00 +0200

    👌 IMPROVE: recipe form

--- O'cooking/src/views/RecipeFormView.vue
+++ O'cooking/src/views/RecipeFormView.vue
@@ -1,9 +1,37 @@
-<script setup>
-import RecipeFormComponent from '../components/Recipe/RecipeFormComponent.vue'
-</script>
+<script setup></script>
 
 <template>
-  <RecipeFormComponent />
+  <div class="main .recipe-form">
+    <h2 class="title">créer une recette</h2>
+    <form>
+      <div>
+        <label class="label" for="title">Nom</label>
+        <input class="input" type="text" name="title" id="title" placeholder="Nom de la recette" />
+      </div>
+
+      <div>
+        <label class="label" for="duration">Temps de cuisson</label>
+        <input class="input" type="text" name="duration" id="duration" placeholder="10 minutes" />
+      </div>
+
+      <div>
+        <label class="label" for="cost">Coût par personne</label>
+        <input class="input" type="text" name="cost" id="cost" placeholder="1€" />
+      </div>
+
+      <div>
+        <label class="label" for="recipe">Ingrédients</label>
+        <p><textarea name="recipe" id="recipe" rows="10"></textarea></p>
+      </div>
+
+      <div>
+        <label class="label" for="recipe">Recette</label>
+        <p><textarea name="recipe" id="recipe" rows="10"></textarea></p>
+      </div>
+
+      <input class="submit" type="submit" value="Sauvegarder" />
+    </form>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
