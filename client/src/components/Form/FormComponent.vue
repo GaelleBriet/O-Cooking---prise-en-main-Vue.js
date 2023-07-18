e6020bb9f69ed1bd03481482818a0f4d9a2f1533
GaelleBriet <gaelle.briet@protonmail.com>
2023-07-18 09:05:22 +0200

    fix

--- client/src/components/Form/FormComponent.vue
+++ client/src/components/Form/FormComponent.vue
@@ -1,6 +1,6 @@
 <script setup>
 import InputComponent from './InputComponent.vue'
-import { useRecipesStore } from '../stores/recipes'
+import { useRecipesStore } from '../../stores/recipes'
 import { useRouter } from 'vue-router'
 import { ref, computed, reactive } from 'vue'
 
@@ -9,10 +9,10 @@ const recipeStore = useRecipesStore()
 
 const props = defineProps({
   form: Array,
-  inputsNumber: Number,
+  inputsNumber: Number
 })
 
-const inputsForm = reactive({});
+const inputsForm = reactive({})
 
 const changeItem = (obj) => {
   inputsForm[obj.name] = obj.value
@@ -21,13 +21,12 @@ const changeItem = (obj) => {
 
 <template>
   <form @submit.prevent="handleSubmit">
-    <InputComponent v-for="(item, i) in form" :key="i" :recipe="item" />
+    <InputComponent v-for="(item, i) in form" :key="i" :recipe="item" @item-change="changeItem" />
     <input class="submit" type="submit" value="Sauvegarder" />
   </form>
 </template>
 
 <style lang="scss" scoped>
-
 input {
   width: 500px;
   padding: 1rem;
