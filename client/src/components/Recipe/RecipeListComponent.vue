eaba9d379733121cf77f843a982a3eaf943e7e4e
GaelleBriet <gaelle.briet@protonmail.com>
2023-07-18 14:48:49 +0200

    📦 NEW:  pagination recipe list

--- client/src/components/Recipe/RecipeListComponent.vue
+++ client/src/components/Recipe/RecipeListComponent.vue
@@ -1,16 +1,40 @@
 <script setup>
 import RecipeCardComponent from './RecipeCardComponent.vue'
 import { useRecipesStore } from '../../stores/recipes'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 
 const recipeStore = useRecipesStore()
 const recipes = computed(() => recipeStore.getRecipes)
 const isLoaded = computed(() => recipeStore.isLoaded)
+
+const currentPage = ref(1)
+const recipesPerPage = 10
+
+// Calcul de l'index de début et de fin des recettes sur la page actuelle
+const startIndex = computed(() => (currentPage.value - 1) * recipesPerPage)
+const endIndex = computed(() => startIndex.value + recipesPerPage)
+
+// Recettes affichées sur la page actuelle
+const displayedRecipes = computed(() => recipes.value.slice(startIndex.value, endIndex.value))
+
+// Fonction pour passer à la page précédente
+const previousPage = () => {
+  if (currentPage.value > 1) {
+    currentPage.value -= 1
+  }
+}
+
+// Fonction pour passer à la page suivante
+const nextPage = () => {
+  if (endIndex.value < recipes.value.length) {
+    currentPage.value += 1
+  }
+}
 </script>
 
 <template>
   <div v-if="isLoaded">
-    <template v-for="recipe in recipes" :key="recipe.id">
+    <template v-for="recipe in displayedRecipes" :key="recipe.id">
       <RecipeCardComponent
         image="https://picsum.photos/400/300?grayscale"
         :imageAlt="recipe.name"
@@ -19,6 +43,11 @@ const isLoaded = computed(() => recipeStore.isLoaded)
         :id="recipe.id"
       />
     </template>
+    <!-- Pagination -->
+    <div>
+      <button @click="previousPage" :disabled="currentPage === 1">Précédent</button>
+      <button @click="nextPage" :disabled="endIndex >= recipes.length">Suivant</button>
+    </div>
   </div>
   <div v-else>
     <p>Chargement des recettes ...</p>
